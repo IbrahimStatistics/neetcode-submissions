a5f4bd59ff99559d087630673f7e6f8818853ec6
Ibrahim Ahmad <IbrahimAhmad21006@gmail.com>
2026-08-19 18:46:06 +0530

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int, int> maps;
+        for(int i = 0; i<nums.size(); i++) 
+        {
+            if(maps.find(nums[i])!=maps.end()) {
+                return true;
+            }
+
+            maps[nums[i]] = i;
+        }
+
+        return false;
+    }
+};
