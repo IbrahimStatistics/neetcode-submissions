de363bb87521d5a18be2e829a94920df9e533030
Ibrahim Ahmad <IbrahimAhmad21006@gmail.com>
2026-09-17 19:42:11 +0530

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1 || n == 2) return n;
+        
+        int first = 1;
+        int second = 2;
+        int next = 0;
+
+        for(int i = 3; i<=n; i++) {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+
+        return next;
+    }
+};
